93a6f5cc4c87306dd3a0b5f702677f0f92bf3634
Luqman Tuke <lsuleiman2002@gmail.com>
2023-07-14 15:20:18 +0300

    styling PDFEdit component

--- src/components/PDFEdit.jsx
+++ src/components/PDFEdit.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useRef } from 'react';
-import { Box, Button, Input, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 
 const PDFEdit = () => {
   const [file, setFile] = useState(null);
@@ -26,54 +35,78 @@ const PDFEdit = () => {
   };
 
   return (
-    <Box>
-      {/* Upload component */}
+    <Container maxW="sm" centerContent py={10}>
       <Box
-        border="2px dashed gray"
-        borderRadius="md"
-        p={4}
-        mb={4}
-        onDrop={(event) => {
-          event.preventDefault();
-          const selectedFile = event.dataTransfer.files[0];
-          setFile(selectedFile);
-        }}
-        onDragOver={(event) => event.preventDefault()}
+        bg="white"
+        boxShadow="md"
+        p={8}
+        rounded="md"
+        w="100%"
+        maxW="md"
       >
-        <input
-          type="file"
-          accept="application/pdf"
-          onChange={handleFileChange}
-          style={{ display: 'none' }}
-          ref={fileInputRef}
-        />
-        <Button colorScheme="gray" onClick={() => fileInputRef.current.click()}>
-          Select PDF File
-        </Button>
-        {file && <p>Selected file: {file.name}</p>}
+        <Stack spacing={6} w="100%">
+          {/* Upload component */}
+          <Box
+            border="2px dashed gray"
+            borderRadius="md"
+            p={4}
+            cursor="pointer"
+            _hover={{ borderColor: 'gray.400' }}
+            onDrop={(event) => {
+              event.preventDefault();
+              const selectedFile = event.dataTransfer.files[0];
+              setFile(selectedFile);
+            }}
+            onDragOver={(event) => event.preventDefault()}
+          >
+            <input
+              type="file"
+              accept="application/pdf"
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+              ref={fileInputRef}
+            />
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              height="120px"
+            >
+              <Button
+                colorScheme="gray"
+                onClick={() => fileInputRef.current.click()}
+              >
+                Select PDF File
+              </Button>
+              {file && (
+                <Text mt={2} fontWeight="bold">
+                  Selected file: {file.name}
+                </Text>
+              )}
+            </Flex>
+          </Box>
+
+          {/* Input field for words to replace */}
+          <Input
+            placeholder="Words to Replace"
+            value={wordsToReplace}
+            onChange={handleWordsToReplaceChange}
+          />
+
+          {/* Input field for replacement words */}
+          <Input
+            placeholder="Replacement Words"
+            value={replacementWords}
+            onChange={handleReplacementWordsChange}
+          />
+
+          {/* Submit button */}
+          <Button colorScheme="blue" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Stack>
       </Box>
-
-      <Stack spacing={4}>
-        {/* Input field for words to replace */}
-        <Input
-          placeholder="Words to Replace"
-          value={wordsToReplace}
-          onChange={handleWordsToReplaceChange}
-        />
-
-        {/* Input field for replacement words */}
-        <Input
-          placeholder="Replacement Words"
-          value={replacementWords}
-          onChange={handleReplacementWordsChange}
-        />
-
-        {/* Submit button */}
-        <Button colorScheme="blue" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </Stack>
-    </Box>
+    </Container>
   );
 };
 
